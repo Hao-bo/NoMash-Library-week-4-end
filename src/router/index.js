bb1994c49e0e5edc01ed49f483c4b5933150dd39
Hao-bo <131502229+Hao-bo@users.noreply.github.com>
2024-10-18 21:24:11 +1100

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,7 @@ const routes = [
 ]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: routes
 })
 router.beforeEach((to, from, next) => {
     // 从 sessionStorage 中读取身份验证状态
